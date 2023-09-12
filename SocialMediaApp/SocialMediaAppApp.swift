827279febf60761a7d8df708e83c66d10c2a4a0f
shruti parmar <parmarshruti26@gmail.com>
2023-09-12 10:35:44 +0530

    Initial Commit

---
+++ SocialMediaApp/SocialMediaAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SocialMediaAppApp.swift
+//  SocialMediaApp
+//
+//  Created by Shruri on 12/09/23.
+//
+
+import SwiftUI
+
+@main
+struct SocialMediaAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
